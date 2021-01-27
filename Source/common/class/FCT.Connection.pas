e72286dd81a3ced97deb4d3543a61be848192265
MARCiOSDEV <marciosdev@icloud.com>
2021-01-26 22:02:14 -0300

    Alter connection class

--- Source/common/class/FCT.Connection.pas
+++ Source/common/class/FCT.Connection.pas
@@ -2,6 +2,41 @@ unit FCT.Connection;
 
 interface
 
+uses
+  System.SysUtils, System.Variants, System.Classes, System.Win.Registry,
+  Uni, PostgreSQLUniProvider, FCT.Setting;
+
+type
+  TFCTConnection = class
+    private
+      FoSetting: TFCTSetting;
+      FoConnect: TUniConnection;
+      procedure AfterConnect;
+    public
+    constructor Create; override;
+    destructor Destroy; override;
+  end;
+
 implementation
+
+{ TFCTConnection }
+
+constructor TFCTConnection.Create;
+begin
+  inherited;
+  FoSetting := TFCTSetting.Create;
+end;
+
+destructor TFCTConnection.Destroy;
+begin
+  if Assigned(FoSetting) then
+    FreeAndNil(FoSetting);
+  inherited;
+end;
+
+procedure TFCTConnection.AfterConnect;
+begin
+
+end;
 
 end.
--- Source/common/class/FCT.Setting.pas
+++ Source/common/class/FCT.Setting.pas
@@ -3,8 +3,8 @@ unit FCT.Setting;
 interface
 
 uses
-System.SysUtils, System.Variants, System.Classes, System.Win.Registry,
-Uni, PostgreSQLUniProvider;
+  System.SysUtils, System.Variants, System.Classes, System.Win.Registry,
+  Uni, PostgreSQLUniProvider;
 
 type
   TFCTSetting = class
