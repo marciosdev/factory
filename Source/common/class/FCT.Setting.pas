59b5c86ec93adb31964dc45be67bbfa73dd671fa
MARCiOSDEV <marciosdev@icloud.com>
2021-01-26 21:47:14 -0300

    Create a Setting Class

---
+++ Source/common/class/FCT.Setting.pas
@@ -0,0 +1,16 @@
+unit FCT.Setting;
+
+interface
+
+uses
+System.SysUtils, System.Variants, System.Classes, System.Win.Registry,
+Uni, PostgreSQLUniProvider;
+
+type
+  TFCTSetting = class
+
+  end;
+
+implementation
+
+end.
